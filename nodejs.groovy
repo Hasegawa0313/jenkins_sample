0bc97a042f04f58d82252b2ff3b00a2e38169c68
Hasegawa0313 <heritakopoochan@gmail.com>
2020-12-17 10:57:06 +0900

    Add DSL

---
+++ nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS Job') {
+    scm {
+        git('https://github.com/Hasegawa0313/jenkins_sample.git') {  node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@example.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs_v12.14.1')
+    }
+    steps {
+        shell("yarn install")
+        shell("yarn test")
+    }
+}
